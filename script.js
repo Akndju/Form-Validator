0622865f0fd27490a0432007cd5297b3b68827bf
Akndju <davidd.akindoju@gmail.com>
2020-07-31 14:07:42 +0100

    fixed registration validation

---
+++ script.js
@@ -0,0 +1,42 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getInputName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+function getInputName(entry) {
+  return entry.id.charAt(0).toUpperCase() + entry.id.slice(1);
+}
+
+function isValidEmail(email) {
+  const regExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regExp.test(String(email).toLowerCase());
+}
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  checkRequired([username, email, password, password2]);
+});
